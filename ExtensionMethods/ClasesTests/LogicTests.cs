672ec7f16788a36468837d2ea838b2dcaca0ee6c
Nahuel Adrián Sosa <sosanahuel021@gmail.com>
2022-06-01 16:51:40 -0300

    Implementando Unit Test en Clase Logic

---
+++ ExtensionMethods/ClasesTests/LogicTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod]
+        public void ExcepcionPunto4Test()
+        {
+            //arrange
+            string expectedMessage = "Lo siento amiguito, ha ocurrido una excepcion. Probando unit test";
+            //act
+            var newException = new ExcepcionPersonalizada("Probando unit test");
+            //Assert
+            Assert.AreEqual(expectedMessage, newException.Message);
+        }
+     
+    }
+}
